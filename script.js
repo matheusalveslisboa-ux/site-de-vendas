5ae5253300256888ad888caec85acc6f9dec8575
matheusalveslisboa-ux <matheus.alves.lisboa@escola.pr.gov.br>
2025-10-20 09:50:51 -0300

    .

--- script.js
+++ script.js
@@ -6,35 +6,35 @@ const products = [
     name: 'Honda CB 500X',
     category: 'moto',
     price: 30000,
-    img: 'https://via.placeholder.com/500x300?text=Honda+CB+500X'
+    img: 'https://www.motociclismo.com.br/wp-content/uploads/2022/04/honda_cb_500x_2022.jpg'
   },
   {
     id: 2,
     name: 'Yamaha MT-07',
     category: 'moto',
     price: 28000,
-    img: 'https://via.placeholder.com/500x300?text=Yamaha+MT-07'
+    img: 'https://www.motociclismo.com.br/wp-content/uploads/2022/02/yamaha_mt07.jpg'
   },
   {
     id: 3,
     name: 'Kawasaki Ninja 400',
     category: 'moto',
     price: 35000,
-    img: 'https://via.placeholder.com/500x300?text=Kawasaki+Ninja+400'
+    img: 'https://www.kawasaki.com.br/uploads/ninja-400-2023.jpg'
   },
   {
     id: 4,
     name: 'Suzuki V-Strom 650',
     category: 'moto',
     price: 36000,
-    img: 'https://via.placeholder.com/500x300?text=Suzuki+V-Strom+650'
+    img: 'https://www.suzuki.com.br/imagens/modelos/vstrom-650-2023.jpg'
   },
   {
     id: 5,
     name: 'BMW G 310 GS',
     category: 'moto',
     price: 26000,
-    img: 'https://via.placeholder.com/500x300?text=BMW+G+310+GS'
+    img: 'https://www.bmw-motorrad.com.br/media/2921/g310gs_home.jpg'
   },
 
   // Carros
@@ -43,35 +43,35 @@ const products = [
     name: 'Fiat Uno 2023',
     category: 'carro',
     price: 50000,
-    img: 'https://via.placeholder.com/500x300?text=Fiat+Uno+2023'
+    img: 'https://quatrorodas.abril.com.br/wp-content/uploads/2022/02/fiat-uno-2022.jpg'
   },
   {
     id: 7,
     name: 'Chevrolet Onix 2023',
     category: 'carro',
     price: 65000,
-    img: 'https://via.placeholder.com/500x300?text=Chevrolet+Onix+2023'
+    img: 'https://quatrorodas.abril.com.br/wp-content/uploads/2021/06/chevrolet-onix-2021.jpg'
   },
   {
     id: 8,
     name: 'Volkswagen Polo 2023',
     category: 'carro',
     price: 75000,
-    img: 'https://via.placeholder.com/500x300?text=Volkswagen+Polo+2023'
+    img: 'https://www.volkswagen.com.br/content/dam/vw/website/experiencia-vw/nossos-modelos/polo/polo2023.jpg'
   },
   {
     id: 9,
     name: 'Toyota Corolla 2023',
     category: 'carro',
     price: 120000,
-    img: 'https://via.placeholder.com/500x300?text=Toyota+Corolla+2023'
+    img: 'https://quatrorodas.abril.com.br/wp-content/uploads/2022/04/toyota-corolla-2023.jpg'
   },
   {
     id: 10,
     name: 'Honda Civic 2023',
     category: 'carro',
     price: 115000,
-    img: 'https://via.placeholder.com/500x300?text=Honda+Civic+2023'
+    img: 'https://quatrorodas.abril.com.br/wp-content/uploads/2022/03/honda-civic-2022.jpg'
   }
 ];
 
