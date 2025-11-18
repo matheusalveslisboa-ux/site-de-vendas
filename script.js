6cfa07adb0f81b29e14b725397f0bede6ae7681e
matheusalveslisboa-ux <matheus.alves.lisboa@escola.pr.gov.br>
2025-11-18 20:42:33 -0300

    .

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ const yearEl = document.getElementById("year");
 
 // PRODUTOS (AGORA SÃO PLANTAS)
 const products = [
-    { id: "1", name: "Bromélia", price: 39.90, img: "https://images.unsplash.com/photo-1611078484309-d3e2f5c7e1ba?w=600" },
-    { id: "2", name: "Cactos", price: 29.90, img: "https://images.unsplash.com/photo-1583827185221-9f0d372013ad?w=600" },
-    { id: "3", name: "Orquídea Phalaenopsis", price: 99.90, img: "https://images.unsplash.com/photo-1616167451437-2fe9d2cfb34d?w=600" },
-    { id: "4", name: "Lavanda", price: 54.90, img: "https://images.unsplash.com/photo-1583612341977-b9a2a383742a?w=600" },
-    { id: "5", name: "Aloe Vera", price: 49.90, img: "https://images.unsplash.com/photo-1597848493920-5d5ca6a7bcfa?w=600" },
-    { id: "6", name: "Rosa do Deserto", price: 79.90, img: "https://images.unsplash.com/photo-1583615990904-13c4e6225fd3?w=600" },
-    { id: "7", name: "Lírio da Paz", price: 69.90, img: "https://images.unsplash.com/photo-1598274849651-6b7e98e9e4f7?w=600" },
+    { id: "1", name: "Bromélia", price: 39.90, img: "https://tse1.mm.bing.net/th/id/OIP.1aIk4JtSaxf31cfj7i3u7QHaE7?rs=1&pid=ImgDetMain&o=7&rm=3" },
+    { id: "2", name: "Cactos", price: 29.90, img: "https://thumbs.dreamstime.com/b/cacto-no-fundo-branco-110935775.jpg" },
+    { id: "3", name: "Orquídea Phalaenopsis", price: 99.90, img: "https://tse3.mm.bing.net/th/id/OIP.TvwTgDxVIojx0T9wM7zN-AHaIh?rs=1&pid=ImgDetMain&o=7&rm=3" },
+    { id: "4", name: "Lavanda", price: 54.90, img: "https://img.freepik.com/fotos-premium/fundo-branco-lavanda-isolado_198067-50.jpg" },
+    { id: "5", name: "Aloe Vera", price: 49.90, img: "https://img.freepik.com/fotos-premium/fundo-branco-da-planta-de-aloe-vera_431161-25023.jpg" },
+    { id: "6", name: "Rosa do Deserto", price: 79.90, img: "https://static.wixstatic.com/media/0f37d7_4960438534fd47bc90ab996c64c6018e~mv2.jpg" },
+    { id: "7", name: "Lírio da Paz", price: 69.90, img: "https://thumbs.dreamstime.com/b/etapa-vegetativa-de-plantas-ervas-maconha-com-fundo-branco-165617426.jpg" },
     
